f8a28f3780942051a6ee2a8f8f370fab5952e99b
sanchigupta12 <sanchigupta447@gmail.com>
2024-04-07 20:13:21 -0400

    EDA and Problems statement

--- src/logger.py
+++ src/logger.py
@@ -14,8 +14,10 @@ logging.basicConfig(
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
     
-    
-)
+    )
+
+if __name__=="__main__":
+    logging.infor("Logging has started")
 
 
     
